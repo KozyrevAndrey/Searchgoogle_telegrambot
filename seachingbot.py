8b8836ac8df5937bae4c7f65bea23565d6d4bb77
KozyrevAndrey <108135223+KozyrevAndrey@users.noreply.github.com>
2022-07-02 16:05:02 +0300

    Create seachingbot.py

---
+++ seachingbot.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from googlesearch import search
+
+
+bot = Bot(token="Your_bot_Token")
+
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Hello!\nWrite me what you want to find and i will send you a couple links to what you are looking for!")
+    
+    
+@dp.message_handler(content_types='text')
+async def search_from_user(message: types.Message):
+    x = search(message.text, lang='eng', stop=3)
+    for i in x:
+        await message.reply(i)
+        
+        
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
